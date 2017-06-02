d823a7a0b6c9b3031f37758a7e2edfb84bef1884
edgargarnica <emga95@hotmail.com>
2017-06-01 18:23:37 -0600

    primer modificacion

--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,35 @@
-angular.module('songhop.services', []);
+angular.module('songhop.services', [])
+.factory('User', function() {
+
+  var o = {
+    favorites: [	
+
+
+    ]
+  }
+    o.addSongToFavorites = function(song) {
+    // make sure there's a song to add
+    if (!song) return false;
+
+    // add to favorites array
+    o.favorites.unshift(song);
+  }
+    o.removeSongFromFavorites = function(song, index) {
+    // make sure there's a song to add
+    if (!song) return false;
+
+    // add to favorites array
+    o.favorites.splice(index, 1);
+  }
+  return o;
+
+})
+.factory('Recommendations', function($http, SERVER) {
+  var o = {
+    queue: []
+  };
+
+  return o;
+});
+
+
