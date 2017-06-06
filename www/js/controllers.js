856154b3c37e7c086d752f1a49218c8ab9872305
edgargarnica <emga95@hotmail.com>
2017-06-06 10:04:16 -0600

    tercera modificacion

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,13 +5,13 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 Controller for the discover page
 */
 .controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations) {
-    Recommendations.getNextSongs()
+    Recommendations.init()
     .then(function(){
       $scope.currentSong = Recommendations.queue[0];
+      Recommendations.playCurrentSong();
     });
 
-
-
+  
 
     $scope.sendFeedback = function (bool) {
     if (bool) User.addSongToFavorites($scope.currentSong);
@@ -22,7 +22,16 @@ Controller for the discover page
       // $timeout to allow animation to complete
       $scope.currentSong = Recommendations.queue[0];
     }, 250);
+    Recommendations.playCurrentSong();
   }
+      $scope.nextAlbumImg = function() {
+    if (Recommendations.queue.length > 1) {
+      return Recommendations.queue[1].image_large;
+    }
+
+    return '';
+  }
+
 })
 
 
@@ -38,6 +47,11 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
-
+.controller('TabsCtrl', function($scope, Recommendations) {
+  $scope.enteringFavorites = function() {
+    Recommendations.haltAudio();
+  }
+    $scope.leavingFavorites = function() {
+    Recommendations.init();
+  }
 });
